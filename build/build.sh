9648539a1404e42d29fbb217344828313e955205
newlee <newlee.cn@gmail.com>
2016-12-19 19:33:09 +0800

    deploy nginx & frontend static files

--- build/build.sh
+++ build/build.sh
@@ -13,7 +13,12 @@ docker cp dev:/go/petstore build/
 
 echo "build production docker image"
 docker rm -f petstore && docker rmi petstore
-docker build -t petstore build/.
+docker build -t petstore -f build/backend/Dockerfile .
 
 echo "docker run"
 docker run -tid --name petstore --publish 8090:8090 petstore
+
+docker rm -f petstore-static && docker rmi petstore-static
+docker build -t petstore-static -f build/frontend/Dockerfile .
+
+docker run -tid --name petstore-static --publish 80:80 petstore-static
