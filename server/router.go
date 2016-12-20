87423ae5532e9d17f79515c8e1d60dcebb180436
newlee <newlee.cn@gmail.com>
2016-12-20 16:28:50 +0800

    empty products api

--- server/router.go
+++ server/router.go
@@ -3,9 +3,23 @@ package server
 import (
 	"github.com/labstack/echo"
 	"net/http"
+
 )
+
+type Product struct {
+
+}
+
+var homeHandler = func(c echo.Context) error {
+	return c.String(http.StatusOK, "Pets store")
+}
+
+var productListHandler = func(c echo.Context) error {
+	products := make([]Product, 0)
+	return c.JSON(http.StatusOK, products)
+}
+
 func Router(e *echo.Echo){
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Pets store")
-	})
+	e.GET("/", homeHandler)
+	e.GET("/products", productListHandler)
 }
--- server/server_test.go
+++ server/server_test.go
@@ -8,13 +8,26 @@ import (
 	"net/http"
 )
 
+var e *echo.Echo
+
 var _ = Describe("Server", func() {
+	BeforeEach(func() {
+		e = echo.New()
+		Router(e);
+	})
+
 	Context("Home", func() {
 		It("should ok", func() {
-			e := echo.New()
-			Router(e);
 			c, _ := request("GET", "/", e)
 			Expect(c).To(Equal(http.StatusOK))
+		})
+	})
+
+	Context("Products", func() {
+		It("should return empty when have not any product", func() {
+			c, b := request("GET", "/products", e)
+			Expect(c).To(Equal(http.StatusOK))
+			Expect(b).To(Equal("[]"))
 		})
 	})
 })
