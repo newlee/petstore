c90862557d91333e8775ec7d004b863455be4b68
newlee <newlee.cn@gmail.com>
2016-12-24 22:38:23 +0800

    make test more readable

--- server/server_suite_test.go
+++ server/server_suite_test.go
@@ -9,6 +9,9 @@ import (
 	"net/http"
 	"github.com/labstack/echo"
 	"strings"
+	"encoding/json"
+
+	. "github.com/newlee/petstore/server"
 )
 
 func TestServer(t *testing.T) {
@@ -26,4 +29,54 @@ func requestWithBody(method, path string, e *echo.Echo, body string) (int, strin
 
 func request(method, path string, e *echo.Echo) (int, string) {
 	return requestWithBody(method, path, e, "")
+}
+
+var huskyOrder = `{"petName":"Husky","guestName":"David","telephone":"123456","address":"address1"}`
+
+var withOrder = func() {
+	c, _ := requestWithBody("POST", "/orders", e, huskyOrder)
+	Expect(c).To(Equal(http.StatusCreated))
+}
+
+var withHusky = func() {
+	c, _ := requestWithBody("POST", "/pets", e, `{"name":"Husky"}`)
+	Expect(c).To(Equal(http.StatusCreated))
+}
+
+type ResponseBody struct {
+	body string
+}
+
+var whenGetAllPets = func() *ResponseBody {
+	c, b := request("GET", "/pets", e)
+	Expect(c).To(Equal(http.StatusOK))
+	return &ResponseBody{body: b}
+}
+
+var whenGetPetByTelephone = func() *ResponseBody {
+	c, b := request("GET", "/orders/123456", e)
+	Expect(c).To(Equal(http.StatusOK))
+	return &ResponseBody{body: b}
+}
+
+var whenGetPetByErrorTelephone = func() int {
+	c, _ := request("GET", "/orders/error", e)
+	return c;
+}
+var thenGetNotFound = func(c int) {
+	Expect(c).To(Equal(http.StatusNotFound))
+}
+func (b *ResponseBody) isEmpty()  {
+	Expect(b.body).To(Equal("[]"))
+}
+
+func (b *ResponseBody) haveHusky()  {
+	pets := make([]Pet, 0)
+	json.Unmarshal([]byte(b.body), &pets)
+	Expect(len(pets)).To(Equal(1))
+	Expect(pets[0].Name).To(Equal("Husky"))
+}
+
+func (b *ResponseBody) isOrderForHusky()  {
+	Expect(b.body).To(Equal(huskyOrder))
 }
--- server/server_test.go
+++ server/server_test.go
@@ -2,15 +2,11 @@ package server_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 	"github.com/labstack/echo"
 	. "github.com/newlee/petstore/server"
-	"net/http"
-	"encoding/json"
 )
 
 var e *echo.Echo
-var orderJson = `{"petName":"Husky","guestName":"David","telephone":"123456","address":"address1"}`
 
 var _ = Describe("Server", func() {
 	BeforeEach(func() {
@@ -18,48 +14,34 @@ var _ = Describe("Server", func() {
 		Router(e);
 	})
 
-	Context("Home", func() {
-		It("should ok", func() {
-			c, _ := request("GET", "/", e)
-			Expect(c).To(Equal(http.StatusOK))
-		})
-	})
-
 	Context("Pets", func() {
 		It("should return empty when have not any pets", func() {
-			c, b := request("GET", "/pets", e)
-			Expect(c).To(Equal(http.StatusOK))
-			Expect(b).To(Equal("[]"))
+			whenGetAllPets().isEmpty()
 		})
 
 		It("should return husky after created.", func() {
-			c, _ := requestWithBody("POST", "/pets", e, `{"name":"Husky"}`)
-			Expect(c).To(Equal(http.StatusCreated))
-
-			c, b := request("GET", "/pets", e)
-			pets := make([]Pet, 0)
-			json.Unmarshal([]byte(b), &pets)
-			Expect(len(pets)).To(Equal(1))
-			Expect(pets[0].Name).To(Equal("Husky"))
+			withHusky()
+			whenGetAllPets().haveHusky()
 		})
 	})
 
 	Context("Order", func() {
 		BeforeEach(func() {
-			requestWithBody("POST", "/pets", e, `{"name":"Husky"}`)
+			withHusky()
 		})
 
 		It("should get order by telephone after created", func() {
-			c, _ := requestWithBody("POST", "/orders", e, orderJson)
-			Expect(c).To(Equal(http.StatusCreated))
+			withOrder()
+			whenGetPetByTelephone().isOrderForHusky()
+		})
 
-			c, b := request("GET", "/orders/123456", e)
-			Expect(b).To(Equal(orderJson))
+		It("should update order when exist", func() {
+
 		})
 
 		It("should get 404 when telephone is error", func() {
-			c, _ := request("GET", "/orders/error", e)
-			Expect(c).To(Equal(http.StatusNotFound))
+			c := whenGetPetByErrorTelephone()
+			thenGetNotFound(c)
 		})
 	})
 })
